6742e31cbd1a2effb581322d900fd1ca6e4bc002
Ayatar <elcikayra@gmail.com>
2024-05-14 23:57:19 +0300

    Added PathControl Project
    Seçtiğimiz bir dosya yolunda adını girdiğimiz dosyanın olup olmadığını kontrol eden akabinde dosya var ise bunu yok ise de dosyayı oluşturup bunun bilgisini döndüren .Net WinForms Projesi

---
+++ PathControl/PathControl/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PathControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(668, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(700, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seçtiğiniz Dosya Yolunda Bir Dosya Var İse Dosya\'yı Oluşturamaz Fakat Dosya Yok İ" +
+    "se Yeni Bir Dosya Oluşturur";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(286, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Dosya Oluştur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(577, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(611, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 268);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ PathControl/PathControl/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathControl
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+        FileDialog fileDialog = new OpenFileDialog();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          string yol = folderDialog.SelectedPath;
+            string[] types = { ".txt", ".docx", ".pdf", ".xlsx", ".pptx" };
+            foreach (var item in types)
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            folderDialog.ShowDialog();
+            textBox1.Text = folderDialog.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string _path = folderDialog.SelectedPath;
+            if (pathControl() == true)
+            {
+                
+                
+
+                File.Create(_path + "\\" + textBox2.Text + comboBox1.SelectedItem);
+                MessageBox.Show("Dosya Oluşturuldu");
+
+            }
+            else
+            {
+                MessageBox.Show("Dosya Oluştrurulamadı");
+            }
+            
+        }
+
+        private  bool pathControl()
+        {
+
+            if (File.Exists(folderDialog.SelectedPath + "\\" + textBox2.Text + comboBox1.SelectedItem) == true)
+            {
+                return false;
+            }
+            else 
+            { 
+                return true;
+            }
+            
+             
+        }
+    }
+}
